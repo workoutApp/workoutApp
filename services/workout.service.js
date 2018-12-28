e6827da90c5ae055c7f4dbbc603a281b75231055
DaveKeith <d.keith78@yahoo.com>
2018-12-27 19:21:38 -0500

    changed app.js to include all the new packages, created a services folder and file

---
+++ services/workout.service.js
@@ -0,0 +1,120 @@
+// Access our newly created Mongoose Model
+var ToDo = require('../models/workout.model.js')
+
+// Let's use an Async function to get the To Do List
+exports.getTodos = async function(query, page, limit){
+
+    // We also want to set up options for the mongoose paginate
+
+    var options = {
+        page,
+        limit
+    }
+
+    //Let's create a Try and Catch function that way we have some error handling set. Waiting for the promise
+        
+    try {
+        var todos = await ToDo.paginate(query, options)
+        
+        //Once the Mongoose promise is returned we're going to go ahead and return the To Do List it has produced 
+
+        return todos;
+
+    } catch (e) {
+
+        //If the try didn't work we're going to go ahead and let the users know what kind of error we have
+
+        throw Error('Oh No! We got an error while Paginating our To-Do Tasks, so sorry!' )
+    }
+}
+
+exports.createTodo = async function(todo){
+    
+    // Creating a new Mongoose Object by using the new keyword
+
+    var newTodo = new ToDo({
+        date: new Date(),
+        exercises: [
+            {
+                name: todo.name,
+                description: todo.description,
+                bodyPart: todo.bodyPart,
+                accomplished: todo.accomplished
+            }
+        ]
+    })
+
+    try{
+
+        // Let's go ahead and save the Todo 
+
+        var savedTodo = await newTodo.save()
+
+        return savedTodo;
+    }catch(e){
+      
+        //if we can't create a Todo we want to throw an error 
+
+        throw Error("Error while Creating Todo")
+    }
+}
+
+exports.updateTodo = async function(todo){
+    var id = todo.id
+
+    try{
+        //Find the old Todo Object by the Id
+    
+        var oldTodo = await ToDo.findById(id);
+    }catch(e){
+        throw Error("Error occured while Finding the Todo")
+    }
+
+    // If no old Todo Object exists return false
+
+    if(!oldTodo){
+        return false;
+    }
+
+    console.log(oldTodo)
+
+    //Edit the Todo Object
+
+    oldTodo.date = new Date();
+    oldTodo.exercises = [
+            {
+                oldTodo.name = todo.name,
+                oldTodo.description = todo.description,
+                oldTodo.bodyPart = todo.bodyPart,
+                oldTodo.accomplished = todo.accomplished
+            }
+        ]
+    oldTodo.title = todo.title
+    oldTodo.description = todo.description
+    oldTodo.status = todo.status
+
+
+    console.log(oldTodo)
+
+    try{
+        var savedTodo = await oldTodo.save()
+        return savedTodo;
+    }catch(e){
+        throw Error("And Error occured while updating the Todo");
+    }
+}
+
+exports.deleteTodo = async function(id){
+    
+    // Delete the Todo
+
+    try{
+        var deleted = await ToDo.deleteOne({_id: id})
+        if(deleted.n === 0){
+            throw Error("Todo Could not be deleted")
+        }
+        return deleted
+    }catch(e){
+        throw Error("Error Occured while Deleting the Todo")
+    }
+}
